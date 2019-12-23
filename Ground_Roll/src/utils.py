e54f6724d69e8c06b3808d5855289ec5c98e9510
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-12-23 14:51:17 +0000

    large dataset study

--- Ground_Roll/src/utils.py
+++ Ground_Roll/src/utils.py
@@ -7,12 +7,12 @@ from seismicpro.batchflow import Pipeline, V, B
 
 from seismicpro.src import FieldIndex, SeismicDataset, seismic_plot
 
-def make_index(paths, index_type=FieldIndex, extra_headers=None):
+def make_index(paths, index_type=FieldIndex, extra_headers=None, index_name=None):
     """ make index given components and paths"""
     return reduce(lambda x, y: x.merge(y), 
-                  (index_type(name=name, path=path, extra_headers=extra_headers) for name, path in paths.items()))
+                  (index_type(name=name, path=path, extra_headers=extra_headers, index_name=index_name) for name, path in paths.items()))
 
-def load_arrs(i, index, components):
+def load_arrs(i, index, components, index_type=FieldIndex, index_name=None):
     """ load i-th field from index"""
     ppl = Pipeline()
     ppl = reduce(lambda p, c: p.init_variable(c), components, ppl)
@@ -21,7 +21,7 @@ def load_arrs(i, index, components):
            .sort_traces(src=components, dst=components, sort_by='offset'))
     ppl = reduce(lambda p, c: p.update(V(c), B(c)), components, ppl)
     
-    findex = FieldIndex(index)
+    findex = index_type(index, index_name=index_name)
     test_set = SeismicDataset(findex.create_subset(findex.indices[i: i+1]))
 
     ppl = ppl << test_set
@@ -29,10 +29,19 @@ def load_arrs(i, index, components):
 
     return {c: ppl.get_variable(c)[0] for c in components}
 
-def check_res(i, index, components, cv=0.1):  
+
+def check_res(i, index, components, mode='img', index_type=FieldIndex, index_name=None, **kwargs):
+    arrs = load_arrs(i, index, components=components, index_type=index_type, index_name=index_name)   
     
-    arrs = load_arrs(i, index, components=components)   
-    seismic_plot(list(arrs.values()), names=list(arrs.keys()),
-                 figsize=(19, 7), cmap='gray', vmin=-cv, vmax=cv, title='Field {}'.format(index.indices[i]))
+    fig_kwargs = {}
+    if mode == 'wiggle':
+        fig_kwargs.update({'wiggle':True, 'xlim':None, 'ylim':None, 'std':1, **kwargs})
+    else:
+        cv = kwargs.pop('cv', 0.1)
+        fig_kwargs.update({'cmap':'gray', 'vmin':-cv, 'vmax':cv, **kwargs})
+        
+    seismic_plot(list(arrs.values()), names=list(arrs.keys()), title='Field {}'.format(index.indices[i]), **fig_kwargs)
     
     return arrs
+
+    
